2706095f73abdc7702d4aa3002764fb0ccf301c6
onder tozan <ondertozann@hotmail.com>
2022-08-05 15:00:44 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
+
+
 
 const App = () => {
   return (
     <div>
-        <h1>GPT-3</h1>
+      <h1>GPT-3</h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
