5b5f73140be47646c025447bfeca3069f7aecee2
onder tozan <ondertozann@hotmail.com>
2022-08-09 16:35:10 +0300

    buttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import Todo from "./components/Todo";
+
 function App() {
-    return <div>Hello!</div>;
-  }
-  
-  export default App;
-  
+  return (
+    <div>
+      <h1>My Todo</h1>
+      <Todo text="Learn react"/>
+      <Todo text="2 react"/>
+      <Todo text="3 react"/>
+    </div>
+  );
+}
+
+export default App;
