636255cb633c7200eb67d951b7975c9d0568cba4
onder tozan <ondertozann@hotmail.com>
2022-08-09 17:27:38 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import Todo from "./components/Todo";
-import Modal from "./components/Modal";
-import Backdrop from "./components/Backdrop";
 
 function App() {
   return (
@@ -9,8 +7,7 @@ function App() {
       <Todo text="Learn react"/>
       <Todo text="2 react"/>
       <Todo text="3 react"/>
-      <Modal />
-      <Backdrop />
+      
     </div>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,18 @@
+import { useState } from "react";
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
+
 function Todo(props) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
   function deleteHandler() {
-    
+    setModalIsOpen(true);
   }
+
+  function closeModalHandler() {
+    setModalIsOpen(false);
+  }
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
@@ -10,6 +21,8 @@ function Todo(props) {
           Delete
         </button>
       </div>
+      {modalIsOpen && <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />}
+      {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
     </div>
   );
 }
