831dee1cc7b6eb5bb78c0c1d0405bb09f0dc8b67
onder tozan <ondertozann@hotmail.com>
2022-08-09 15:34:53 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
-import React from "react";
-import {BrowserRouter as Router, Link, Route, Routes} from 'react-router-dom';
-
-import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from "./containers";
-import { CTA, Brand, Navbar } from "./components";
-import './App.css';
-
-const App = () => {
-  return (
-    <div className="App">
-      <div className="gradient__bg">
-        <Navbar />
-        <Header />
-      </div>
-      <Brand />
-      <Blog />
-    </div>
-  );
-};
-
-export default App;
+function App() {
+    return <div>Hello!</div>;
+  }
+  
+  export default App;
+  
--- src/components/brand/import.js
+++
@@ -1,9 +0,0 @@
-import java from '../..//assets/java.png';
-import springboot from '../..//assets/springboot.png';
-import reactjs from '../..//assets/reactjs.png';
-
-export {
-    java,
-    springboot,
-    reactjs,
-}
