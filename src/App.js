86f0fa4f0ee5d96383b569cbd31a84c5cddd9ca6
onder tozan <ondertozann@hotmail.com>
2022-08-10 10:58:22 +0300

    navigation

--- src/App.js
+++ src/App.js
@@ -3,21 +3,24 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupsPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
     <div>
-      <Switch>
-        <Route exact path="/">
-          <AllMeetupsPage />
-        </Route>
-        <Route exact path="/new-meetup">
-          <NewMeetupsPage />
-        </Route>
-        <Route exact path="/favorites">
-          <FavoritesPage />
-        </Route>
-      </Switch>
+      <MainNavigation>
+        <Switch>
+          <Route exact path="/">
+            <AllMeetupsPage />
+          </Route>
+          <Route exact path="/new-meetup">
+            <NewMeetupsPage />
+          </Route>
+          <Route exact path="/favorites">
+            <FavoritesPage />
+          </Route>
+        </Switch>
+      </MainNavigation>
     </div>
   );
 }
