1f17e095af4a4128a796ddd2e7838e4dd8c3cbda
onder tozan <ondertozann@hotmail.com>
2022-08-10 18:07:07 +0300

    routes created

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const App = () => {
               className="w-72 fixed sidebar
             dark:bg-secondary-dar-bg bg-white"
             >
-              sidebar
+              navbar
             </div>
           ) : (
             <div
@@ -53,7 +53,30 @@ const App = () => {
           </div>
           <div>
             <Routes>
+              {/*Dashboard */}
               <Route path="/" element="ECommerce" />
+              <Route path="/ecommerce" element="ECommerce" />
+
+              {/*Pages */}
+              <Route path="/orders" element="Orders" />
+              <Route path="/employees" element="Employees" />
+              <Route path="/customers" element="Customers" />
+
+              {/*Apps*/}
+              <Route path="/kanban" element="Kanban" />
+              <Route path="/editor" element="Editor" />
+              <Route path="/calendar" element="Calendar" />
+              <Route path="/color-picker" element="ColorPicker" />
+
+              {/*Charts*/}
+              <Route path="/line" element="Line" />
+              <Route path="/area" element="Area" />
+              <Route path="/bar" element="Bar" />
+              <Route path="/pie" element="Pie" />
+              <Route path="/financial" element="Financial" />
+              <Route path="/color-mapping" element="ColorMapping" />
+              <Route path="/pyramid" element="Pyramid" />
+              <Route path="/stacked" element="Stacked" />
             </Routes>
           </div>
         </div>
